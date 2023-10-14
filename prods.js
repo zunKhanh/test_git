d04087dbe494854b36d097128c65544e606db3b8
GIGA\T.Khanh <hdtk21122004@gmail.com>
2023-10-14 16:06:49 +0700

    no message

---
+++ prods.js
@@ -0,0 +1,56 @@
+var list = [
+    {
+        "name": "Máy tính DELL",
+        "price": 1000,
+        "discount": 0.19,
+        "date": "2022-06-09"
+    },
+    {
+        "name": "Máy tính HP",
+        "price": 1200,
+        "discount": 0.5,
+        "date": "2022-12-09"
+    },
+    {
+        "name": "Máy tính Lenovo",
+        "price": 1500,
+        "discount": 0.7,
+        "date": "2022-03-09"
+    },
+    {
+        "name": "Máy tính Apple",
+        "price": 2000,
+        "discount": 2.0,
+        "date": "2022-05-07"
+    },
+    {
+        "name": "Máy tính Acer",
+        "price": 1200,
+        "discount": 0.3,
+        "date": "2022-05-06"
+    },
+    {
+        "name": "Máy tính ASUS",
+        "price": 2000,
+        "discount": 0.4,
+        "date": "2022-05-05"
+    },
+    {
+        "name": "Máy tính LG",
+        "price": 1500,
+        "discount": 0.5,
+        "date": "2022-05-08"
+    },
+    {
+        "name": "Máy tính Huawei",
+        "price": 1600,
+        "discount": 0.5,
+        "date": "2022-05-01"
+    },
+    {
+        "name": "Máy tính Gigabyte",
+        "price": 1700,
+        "discount": 0.6,
+        "date": "2022-05-11"
+    }
+]
